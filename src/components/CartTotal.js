bfaeaab68b96116de35f0ffd3b54a2ccf6bf6e86
NS918326 <Navrajgaltee@outlook.com>
2024-03-03 14:04:49 -0500

    CHNG: format total price value

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CartTotal.css";
+import { NumericFormat } from "react-number-format";
 
 const CartTotal = ({ items }) => {
   const getTotalPrice = () => {
@@ -8,17 +9,27 @@ const CartTotal = ({ items }) => {
     // Add the price to the quantity to the total price
     // return the total price
     let total = 0;
+    let quantityTotal = 0;
     items.forEach((item) => {
       total += item.price * item.quantity;
+      quantityTotal += item.quantity;
     });
-    return total;
+    return { total, quantityTotal };
   };
 
   return (
     <div className="CartTotal">
       <h3>
-        Subtotal({items.length} items):
-        <span className="CartTotal-price">${getTotalPrice()}</span>
+        Subtotal({getTotalPrice().quantityTotal} items):
+        <span className="CartTotal-price">
+          <NumericFormat
+            value={getTotalPrice().total}
+            displayType="text"
+            thousandSeparator={true}
+            prefix="$"
+            decimalScale={2}
+          />
+        </span>
       </h3>
       <button>Proceed to checkout</button>
     </div>
