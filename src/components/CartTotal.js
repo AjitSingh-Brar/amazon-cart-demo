601a3e49d364afbebe434c55d48299112a62fa15
NS918326 <Navrajgaltee@outlook.com>
2024-03-03 13:53:49 -0500

    ADD: calc total price

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./CartTotal.css";
 
-const CartTotal = () => {
+const CartTotal = ({ items }) => {
+  const getTotalPrice = () => {
+    //  init total price to 0
+    // loop through all the iem sin the cart
+    // Add the price to the quantity to the total price
+    // return the total price
+    let total = 0;
+    items.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
+
   return (
     <div className="CartTotal">
       <h3>
-        Subtotal(5 items):
-        <span className="CartTotal-price">$2,172.38</span>
+        Subtotal({items.length} items):
+        <span className="CartTotal-price">${getTotalPrice()}</span>
       </h3>
       <button>Proceed to checkout</button>
     </div>
